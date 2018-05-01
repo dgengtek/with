2d09e4a6a72bafbef6660b7075a6ead9a0199d64
dgengtek <dgengtek@users.noreply.github.com>
2018-05-01 16:16:50 +0200

    Parse commands with shlex

--- withtool/main.py
+++ withtool/main.py
@@ -5,13 +5,13 @@ from docopt import docopt
 
 import withtool
 from withtool.prompt import get_prompt
-from withtool.subprocess import run
+from withtool.subprocess import run, parse_command
 
 
 @asyncio.coroutine
 def main():
     """
-Usage: with (--version | <command>)
+Usage: with (--version | [--] <command> ...)
 
 Arguments:
     command     The command to use as prefix to your context.
@@ -26,7 +26,13 @@ Options:
         print('with {}'.format(withtool.__version__))
         sys.exit()
 
+    command = parse_command(arguments['<command>'])
+
+    prompt = " ".join(command)
+
     while True:
-        sub = yield from get_prompt(arguments['<command>'])
-        call = '{cmd} {sub}'.format(cmd=arguments['<command>'], sub=sub)
+        call = list(command)
+        sub = yield from get_prompt(prompt)
+        if sub:
+            call.append(sub)
         run(call)
--- withtool/subprocess.py
+++ withtool/subprocess.py
@@ -1,8 +1,16 @@
 import subprocess
+import shlex
 
 
 def run(command):
     try:
-        subprocess.check_call(command, shell=True)
+        subprocess.check_call(command)
     except:
         pass
+
+
+def parse_command(command):
+    commands = list()
+    for cmd in command:
+        commands.extend(shlex.split(cmd))
+    return commands
